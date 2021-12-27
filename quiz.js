f029b1a8857d4cbbc5925d65e15fe342767e0eac
mdavie <marianahodniki1991@gmail.com>
2021-12-27 15:42:35 -0700

    added css and fixes html - add all variables

---
+++ quiz.js
@@ -0,0 +1,32 @@
+var myQuestions = [
+    {
+        title:"What does HTML stand for?",
+        choices:["A. Hyper Trainer Marking Language", 
+        "B. Hyper Text Marketing Language", 
+        "C. Hyper Text Markup Language",
+        "D. Hyper Text Markup Leveler"],
+        correctAnswer: 0
+    
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
+        correctAnswer: 0
+    },
+    {
+        title: "The first index of an array is ____.",
+        choices: ["A. 0", "B. 1", "C. 8", "D. any"],
+        correctAnswer: 0
+    },
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["A. onclick", "B. onchange", "C. onmouseover", "D. onmouseclick"],
+        correctAnswer: 0
+    },
+    {
+        title: "How do you add a comment in a JavaScript?",
+        choices: ["A. //This is a comment", "B. <!--This is a comment-->", "C. 'This is a comment", "D. * This is a comment *"],
+        correctAnswer: 0
+    },
+    
+    ];
