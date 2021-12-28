8aed733b669566f32715dd1802f064e649e49675
mdavie <marianahodniki1991@gmail.com>
2021-12-28 09:09:12 -0700

    change wrong variable fixed

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,6 @@
 var myQuestions = [
     {
-        title:"What does HTML stand for?",
+        question:"What does HTML stand for?",
         choices:["A. Hyper Trainer Marking Language", 
         "B. Hyper Text Marketing Language", 
         "C. Hyper Text Markup Language",
@@ -9,24 +9,25 @@ var myQuestions = [
     
     },
     {
-        title: "String values must be enclosed within _____ when being assigned to variables.",
+        question: "String values must be enclosed within _____ when being assigned to variables.",
         choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
         correctAnswer: 0
     },
     {
-        title: "The first index of an array is ____.",
+        question: "The first index of an array is ____.",
         choices: ["A. 0", "B. 1", "C. 8", "D. any"],
         correctAnswer: 0
     },
     {
-        title: "Which event occurs when the user clicks on an HTML element?",
+        question: "Which event occurs when the user clicks on an HTML element?",
         choices: ["A. onclick", "B. onchange", "C. onmouseover", "D. onmouseclick"],
         correctAnswer: 0
     },
     {
-        title: "How do you add a comment in a JavaScript?",
+        question: "How do you add a comment in a JavaScript?",
         choices: ["A. //This is a comment", "B. <!--This is a comment-->", "C. 'This is a comment", "D. * This is a comment *"],
         correctAnswer: 0
     },
     
     ];
+
